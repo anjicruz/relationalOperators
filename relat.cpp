ff6f8befb6a52701aa5b8a5cda7f7ca6f8b05483
anji <anji_cruz@msn.com>
2024-02-06 07:16:16 +0800

    Add project files.

---
+++ relat.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+    int numb;
+    std::cout << "Enter a number: ";
+    std::cin >> numb;
+    std::cout << "numb<10 is " << (numb < 10) << std::endl;
+    std::cout << "numb>10 is " << (numb > 10) << std::endl;
+    std::cout << "numb==10 is " << (numb == 10) << std::endl;
+}
+
+
